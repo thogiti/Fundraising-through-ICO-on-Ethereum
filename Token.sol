6bd8fdff01a6e79f9f52176a02814d657aa9aa5a
Nagu Thogiti <thogiti@gmail.com>
2017-07-16 21:50:49 -0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,5 +1,5 @@
-pragma solidity 0.4.8;
- 
+pragma solidity 0.4.11;
+
 contract admined {
 	address public admin;
 
@@ -7,11 +7,13 @@ contract admined {
 		admin = msg.sender;
 	}
 
+  //Only Admin can access
 	modifier onlyAdmin(){
 		if(msg.sender != admin) throw;
 		_;
 	}
 
+  //Only Admin can transfer membership to someone
 	function transferAdminship(address newAdmin) onlyAdmin {
 		admin = newAdmin;
 	}
@@ -19,13 +21,21 @@ contract admined {
 }
 
 contract Token {
+  /// ERC 20 tokens
+  /// Token smart contract to exchange ETH to tokens
+
+
+  /// from The address of the sender
+  /// to The address of the recipient
+  /// value The amount of token to be transferred
+
 
 	mapping (address => uint256) public balanceOf;
-	// balanceOf[address] = 5;
-	string public name;
-	string public symbol;
-	uint8 public decimal; 
-	uint256 public totalSupply;
+
+	string public name; ///token name
+	string public symbol; ///token symbol
+	uint8 public decimal;
+	uint256 public totalSupply; /// total amount of tokens
 	event Transfer(address indexed from, address indexed to, uint256 value);
 
 
@@ -38,9 +48,10 @@ contract Token {
 	}
 
 	function transfer(address _to, uint256 _value){
+    //Validate the funds available for Transfer
+
 		if(balanceOf[msg.sender] < _value) throw;
 		if(balanceOf[_to] + _value < balanceOf[_to]) throw;
-		//if(admin)
 
 		balanceOf[msg.sender] -= _value;
 		balanceOf[_to] += _value;
@@ -49,20 +60,28 @@ contract Token {
 
 }
 
-contract AssetToken is admined, Token{
+contract EITOToken is admined, Token{
+  //Create EITO Asset Token contract for initial minting of tokens
+  //extends Token contract
 
+	string public name = 'EITO Token'; ///token name
+	string public symbol = '8ET'; ///token symbol
 
-	function AssetToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address centralAdmin) Token (0, tokenName, tokenSymbol, decimalUnits ){
-		totalSupply = initialSupply;
+	function EITOToken(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits, address centralAdmin) Token (0, tokenName, tokenSymbol, decimalUnits ){
+    //Initialize the token supply
+
+  	totalSupply = initialSupply;
 		if(centralAdmin != 0)
 			admin = centralAdmin;
 		else
 			admin = msg.sender;
 		balanceOf[admin] = initialSupply;
-		totalSupply = initialSupply;	
+		totalSupply = initialSupply;
 	}
 
 	function mintToken(address target, uint256 mintedAmount) onlyAdmin{
+    //mint the tokens
+
 		balanceOf[target] += mintedAmount;
 		totalSupply += mintedAmount;
 		Transfer(0, this, mintedAmount);
@@ -70,19 +89,15 @@ contract AssetToken is admined, Token{
 	}
 
 	function transfer(address _to, uint256 _value){
+    //Validate the funds available for transfer
+
 		if(balanceOf[msg.sender] <= 0) throw;
 		if(balanceOf[msg.sender] < _value) throw;
 		if(balanceOf[_to] + _value < balanceOf[_to]) throw;
-		//if(admin)
+
 		balanceOf[msg.sender] -= _value;
 		balanceOf[_to] += _value;
 		Transfer(msg.sender, _to, _value);
 	}
 
 }
-
-
-
-
-
-
